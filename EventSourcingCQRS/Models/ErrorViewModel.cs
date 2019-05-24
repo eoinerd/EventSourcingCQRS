4c6dbbfdc2e39961d27da3d5751ef82f0c243ef4
Eoin Dennehy <EDennehy@internal.mywebgrocer.com>
2019-05-24 10:27:54 +0100

    first commit

---
+++ EventSourcingCQRS/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSourcingCQRS.Models
+{
+    public class ErrorViewModel
+    {
+        public bool ShowRequestId { get; set; }
+
+        public string RequestId { get; set; }
+    }
+}
